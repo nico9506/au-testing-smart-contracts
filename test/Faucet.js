8d0b794660a010931c852bf197cb1f8679f7ec9f
Nicolas Lopez <niclopezt@gmail.com>
2025-07-20 13:15:08 +0700

    set up initial config
    create Faucet contract to test
    basic test case proved

---
+++ test/Faucet.js
@@ -0,0 +1,25 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Fuacet", () => {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployContractAndSetVariables() {
+    const Faucet = await ethers.getContractFactory("Faucet");
+    const faucet = await Faucet.deploy();
+
+    const [owner] = await ethers.getSigners();
+
+    console.log("Signer 1 address: ", owner.address);
+
+    return { faucet, owner };
+  }
+
+  it("Should deploy and set owner correctly", async () => {
+    const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
+
+    expect(await faucet.owner()).to.equal(owner.address);
+  });
+});
